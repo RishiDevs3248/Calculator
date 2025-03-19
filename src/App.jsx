e7e66ebe6db6caa95ca3739f8dc03e0fe5a19a84
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-03-19 13:06:41 +0530

    [Update -> UI skeletion]

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [inputVal,setInputVal] = useState('')
-  const handleClick =(e)=>{
+  let [inputVal, setInputVal] = useState('')
+  const handleClick = (e) => {
     setInputVal(inputVal += e.target.value)
     console.log((inputVal))
   }
 
-  const handleEval =()=>{
+  const handleEval = () => {
     try {
       setInputVal(eval(inputVal))
-    }catch (error) {
+    } catch (error) {
       setInputVal('ERROR')
     }
   }
 
-  const handleBack =()=>{
+  const handleBack = () => {
     try {
       setInputVal(inputVal.slice(0, -1))
-    }catch (error) {
+    } catch (error) {
       setInputVal('ERROR')
     }
   }
@@ -30,8 +30,36 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-900'>Hello Calculator</div>
-      <div>
+      <div className='h-screen w-screen'>
+        <nav className='bg-white flex justify-between h-[5%] px-[20px] items-center sticky top-0 border-b-[0.5px]'>
+          <div>Calculator</div>
+          <div>
+            Dark mode
+          </div>
+        </nav>
+
+        <div className='bg-white h-[95%] w-full flex justify-center items-center '>
+          {/* card */}
+          <div className='bg-[#ECECED] min-h-[700px] h-[90%] min-w-[600px] w-[40%] rounded-3xl shadow-lg'>
+            {/* Screen */}
+            <div className='h-[30%]'>
+              <input type='text' value={inputVal}></input>
+            </div>
+            {/* Buttons */}
+            <div className='bg-[#FCFCFC] h-[70%] rounded-3xl'>
+              <button className='bg-[#FDFDFD] h-[50px] w-[50px] shadow-md'>
+                1
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+{/* <div>
         <input type='text' value={inputVal}></input>
       </div>
 
@@ -53,9 +81,6 @@ function App() {
         <button onClick={()=>{handleEval()}} value={'='}>=</button>
         <button onClick={(e)=>{setInputVal('')}} value={'C'}>clear</button>
         <button onClick={(e)=>{handleBack()}} value={'B'}>Back</button>
-      </div>
-    </>
-  )
-}
+      </div> */}
 
 export default App
