ad0faa5670513edbda9dfabd976f33fbae78a69a
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-03-24 15:05:46 +0530

    [Simple ui done]

--- src/App.jsx
+++ src/App.jsx
@@ -32,54 +32,52 @@ function App() {
   return (
     <>
       <div className='h-screen w-screen'>
-        <nav className='bg-white flex justify-between h-[5%] px-[20px] items-center sticky top-0 border-b-[0.5px]'>
+        {/* <nav className='bg-white flex justify-between h-[5%] px-[20px] items-center sticky top-0 border-b-[0.5px]'>
           <div>Calculator</div>
           <div>
             Dark mode
           </div>
-        </nav>
+        </nav> */}
 
         <div className='bg-white h-[95%] w-full flex justify-center items-center '>
           {/* card */}
-          <div className='bg-[#ECECED] min-h-[600px] h-[600px] min-w-[400px] w-[400px] rounded-3xl shadow-lg'>
+          <div className='bg-[#3B4047] min-h-[600px] h-[600px] min-w-[400px] w-[400px] rounded-3xl shadow-lg drop-shadow-3xl'>
             {/* Screen */}
-            {/* Set this --------------------------- */}
-            <div className='h-[30%] rounded-[25%] bg-red-500'>
-
-              <input type='text' className='bg-green-500 ' value={inputVal}></input>
+            <div className='h-[30%] flex justify-center flex-col items-center'>
+              <input type='text' className='bg-white h-[50%] w-[80%] rounded-full text-right px-[30px] text-2xl shadow-3xl' value={inputVal}></input>
             </div>
             {/* Buttons */}
-            <div className='bg-[#FCFCFC] h-[70%] rounded-3xl p-[20px] flex flex-col justify-evenly'>
+            <div className='bg-[#3B4047] h-[70%] rounded-3xl p-[20px] pt-[0px] flex flex-col justify-evenly'>
               <div className='flex justify-evenly'>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
+                <Button text="C" value="C" Color="text-[#93E9C7]" onClick={(e)=>{setInputVal('')}}></Button>
+                <Button text="M" value="M" Color="text-[#93E9C7]" onClick={(e)=>{handleBack()}}></Button>
+                <Button text="%" value="%" Color="text-[#93E9C7]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="/" value="/" Color="text-[#93E9C7]" onClick={(e)=>{handleClick(e)}}></Button>
               </div>
               <div className='flex justify-evenly'>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
+                <Button text="7" value="7" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="8" value="8" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="9" value="9" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="x" value="*" Color="text-[#93E9C7]" onClick={(e)=>{handleClick(e)}}></Button>
               </div>
               <div className='flex justify-evenly'>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
+                <Button text="4" value="4" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="5" value="5" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="6" value="6" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="-" value="-" Color="text-[#93E9C7]" onClick={(e)=>{handleClick(e)}}></Button>
               </div>
               <div className='flex justify-evenly'>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
+                <Button text="1" value="1" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="2" value="2" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="3" value="3" Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="+" value="+" Color="text-[#93E9C7]" onClick={(e)=>{handleClick(e)}}></Button>
               </div>
               <div className='flex justify-evenly'>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
-                <Button text="1" value="1" Color="text-red-500"></Button>
+                <Button text="0" value="0" Color="text-[#A1A0A4]" width='b' onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="," value="," Color="text-[#A1A0A4]" onClick={(e)=>{handleClick(e)}}></Button>
+                <Button text="=" value="=" Color="text-[#93E9C7]" onClick={()=>{handleEval()}}></Button>
               </div>
+              
 
             </div>
           </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
-export default function Button({Color, value , text}){
+export default function Button({ Color, value, text, onClick, width = "s" }) {
     return (
-        <button className={`bg-transparent h-[50px] w-[50px] ${Color}`} value={value}>
+        <button 
+            className={`bg-transparent h-[50px] ${width == "b" ? "w-[140px]" : "w-[50px]"} text-3xl ${Color}`} 
+            value={value} 
+            onClick={onClick}
+        >
             {text}
         </button>
-    )
+    );
 }
